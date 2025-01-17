cb8a2ba75f1ae2d6684c5fced6c71d23e26de4e0
BANDI VARUN SAI <117831672+JustVarunsai@users.noreply.github.com>
2025-01-17 10:01:34 +0530

    Add files via upload

---
+++ LE_01.java
@@ -0,0 +1,42 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+
+public class Main {
+    private String name;
+    private int age;
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for name
+    public String getName() {
+        return this.name;
+    }
+
+    // Setter for age
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Getter for age
+    public int getAge() {
+        return this.age;
+    }
+
+    public static void main(String[] args) {
+        Main m1 = new Main();
+        m1.setName("Ajay");
+        m1.setAge(18);
+        System.out.println(m1.getName() + " " + m1.getAge());
+    }
+}
+
